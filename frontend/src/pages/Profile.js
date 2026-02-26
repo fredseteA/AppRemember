ce7d313c02e976129dd29462583404135506aece
Frederico Luna <fredericoluna93@gmail.com>
2026-02-26 18:25:14 -0300

    fix: eslint warning Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -34,6 +34,7 @@ const Profile = () => {
     if (user) {
       fetchUserData();
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   const fetchUserData = async () => {
